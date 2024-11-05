b9a6b5ebd26e56caea29727d69996677724628a5
Alexander Rudometkin <rudialex@gmail.com>
2024-11-06 00:12:13 +0300

    New files added

--- HW01_A0_A1_A2_A3/task_A3.c
+++ HW01_A0_A1_A2_A3/task_A3.c
@@ -1,8 +1,15 @@
 #include <stdio.h>
-int main(void)
+#include <inttypes.h>
+
+int main()
 {
-    int a,b,c;
-    scanf("%d %d %d",&a,&b,&c);
-    printf("%d+%d+%d=%d",a,b,c,a+b+c);
-    return 0;
+    unsigned int mask, n=0xAB; // n = 1010 1011
+    printf("n = %x\n", n);
+    mask = (1<<5)-1;
+    //~ mask = 0x1F;
+    //~ mask = 0b11111;
+    n = n ^ mask;
+    printf("n = %x\n", n);
+    // n = b4 1011 0100
 }
+
